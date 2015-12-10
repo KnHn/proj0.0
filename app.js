aefa5861aa178b909c310530b54aa056c358b391
KnHn <kevinhh@uci.edu>
2015-12-09 18:13:05 -0800

    openshift fix

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ var app = express();
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
+http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+    server();
+});
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
